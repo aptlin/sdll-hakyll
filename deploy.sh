bf74072658709536134114b6ca1e5e45b195fdde
sdll <shura.sdll@gmail.com>
2017-03-21 13:01:25 +0000

    edited deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -22,10 +22,11 @@ git commit -m "$1" || error\_exit "deploy script error: no changes to commit"
 
 # Switch to master branch
 git checkout master
+cd $BLOG_DIR
 
 # delete old site
 rm -rf !(.|..|.git|.gitignore|.nojekyll|.stack-work|_cache|_site)
-mv _site/* ..
+mv _site/* $BLOG_DIR/
 git add -A
 git commit -m "$1"
 git push origin master
